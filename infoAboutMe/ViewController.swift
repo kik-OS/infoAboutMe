26780e445545222f68a95598a916c49d868242de
Nikita Gvozdikov <pounddnuop@mail.ru>
2020-10-23 09:02:42 +0300

    Initial Commit

---
+++ infoAboutMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  infoAboutMe
+//
+//  Created by Никита Гвоздиков on 23.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
